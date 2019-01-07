8e0066e6a4fa695b2f18230bbec2f41d9940d011
Bilal Khan <mohammedbilalkhan95@gmail.com>
2019-01-07 17:59:55 +0000

    initial commit 07/01/2019

---
+++ src/main/scala/my_gamora.scala
@@ -0,0 +1,12 @@
+object my_gamora extends App{
+  def add() {
+    var a = 7
+    var b = 9
+
+    var sum = 0
+
+    sum = a + b
+    print(sum)
+  }
+  add()
+}
---
+++ src/main/scala/test.scala
@@ -0,0 +1,7 @@
+object test extends App{
+
+  def test_1(): Unit = {
+    println("Hello World, Bilal")
+  }
+  test_1()
+}
