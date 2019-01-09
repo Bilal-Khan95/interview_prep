718e5e6be535e46604702256ed51a39f683e4e2a
Bilal Khan <mohammedbilalkhan95@gmail.com>
2019-01-09 17:35:30 +0000

    wednesday tasks

--- src/main/scala/AnagramMaker.scala
+++ src/main/scala/AnagramMaker.scala
@@ -1,36 +1,72 @@
+
+
 object AnagramMaker extends App {
-
-  import scala.io.StdIn.readLine
+  import scala.io.StdIn.{readLine, readInt}
   import scala.io.Source
-
-  println("Enter some letters (max 4)")
+//
+//  println("Enter a number")
+//  var numInput = readInt
+  println("Enter 4 letters")
   var wordInput = readLine.toArray
   val wordSource = Source.fromFile("C:/Users/Admin/Documents/Bilals Stuff/Bilal - Scala/enable1.txt").getLines.toList
-  var i = 0
-  var k = 0
-  var count = 0
 
-  def wordFinder(word:Array[Char]): Unit ={
-    while(i < wordSource.length) {
-      while (count < word.length) {
-        if (wordSource(i).contains(word(k))) {
-          k += 1
-          count += 1
-        } else {
-          count += 1
-        }
-      }
-      if (k == word.length) {
-        println(wordSource(i))
-        i += 1
-        k = 0
-        count = 0
-      } else {
-        i += 1
-        k = 0
-        count = 0
-      }
+  for(i <- wordSource.indices){
+    if(wordSource(i).contains(wordInput)){
+      val test = wordSource(i)
+      print(test)
     }
   }
-  val result1 = wordFinder(wordInput).filter.contains
+
+//  val listOfWords = List("apple", "helloeeeeeeeeeeeeeeeee", "aaaaaaaalep", "appleee")
+//  val keyboardKeys = "alep".toArray
+//  var longestWord = ""
+//  checkWord()
+//  def checkWord():Unit = {
+//    for (word <- wordSource) {
+//      var containsLetters = ArrayBuffer[Boolean]()
+//      for (letter <- wordInput) {
+//        if (word.contains(letter)) containsLetters.append(true)
+//        else containsLetters.append(false)
+//      }
+//      if (containsLetters.exists(value => value) && longestWord.length < word.length) {
+//        longestWord = word
+//      }
+//    }
+//    println(longestWord)
+//  }
+//  def wordFinder(word:Array[Char]){
+//
+//    var i = 0
+//    var k = 0
+//    var count = 0
+//    while(i < wordSource.length) {
+//      while (count < word.length) {
+//        if (wordSource(i).contains(word(k))) {
+//          k += 1
+//          count += 1
+//        } else {
+//          count += 1
+//        }
+//      }
+//      if (k == word.length) {
+//        println(wordSource(i))
+//        i += 1
+//        k = 0
+//        count = 0
+//      } else {
+//        i += 1
+//        k = 0
+//        count = 0
+//      }
+//    }
+//  }
+
+//  var b = 0
+//  while(b < numInput){
+//    wordFinder(wordInput)
+//    b +=1
+//  }
+//  var test: String = ""
+
+
 }
